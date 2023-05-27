a3ffea7cee52e0506b9a96e3518a6c8a76817cac
veronicacheung <veronica.cheung1105@gmai.com>
2023-05-27 13:34:57 -0700

    Initial Commit

---
+++ imagePickerApp/imagePickerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  imagePickerAppApp.swift
+//  imagePickerApp
+//
+//  Created by Veronica Cheung on 5/27/23.
+//
+
+import SwiftUI
+
+@main
+struct imagePickerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
